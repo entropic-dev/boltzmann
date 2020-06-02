821abbd33585446ea98725258a4ac369c8024327
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-06-02 13:17:28 -0700

    Add tupleResponse decorator. Add custom-middleware example with tests.
    This should give end users a good survey over middleware testing
    techniques in Boltzmann, as well as some examples of implementing
    real-world middleware. Not that we'd recommend gzipping in Node
    in production or anything.
    
    Enables tests in the GitHub action.

--- examples/custom-middleware/handlers.js
+++ examples/custom-middleware/handlers.js
@@ -1,10 +1,33 @@
-const { Context } = require('./boltzmann.js'); // optionally pull in typescript definition
+'use strict'
 
-greeting.route = 'GET /hello/:name'
-async function greeting(/** @type {Context} */ context) {
-    return `hello ${context.params.name}`
+const { Context } = require('./boltzmann.js')
+
+define.route = 'GET /:query'
+async function define(/** @type {Context} */ context) {
+
+  const q = context.params.query
+  const response = await context.myClient.get('/', {
+    params: {
+      q,
+      format: 'json'
+    }
+  })
+
+  const abstract = response.data.Abstract || response.data.AbstractText
+
+  if (abstract) {
+    return abstract
+  }
+
+  // plain-text responses with a statuscode or headers are a little
+  // unwieldy: you have to turn the string into a buffer first.
+  // 
+  // In the 200 case, they don't have to be turned into a buffer.
+  return Object.assign(Buffer.from(`Could not find any info about "${q}"`), {
+    [Symbol.for('status')]: 404
+  })
 }
 
 module.exports = {
-    greeting
+  define
 }
---
+++ examples/custom-middleware/middleware/attach-client.js
@@ -0,0 +1,21 @@
+'use strict'
+
+module.exports = attachClient
+
+const axios = require('axios')
+
+function attachClient ({ url }) {
+  return next => {
+    // this part only runs once!
+    const client = axios.create({
+      baseURL: url,
+      timeout: 1000
+    })
+
+    return async context => {
+      // this part runs on every request!
+      context.myClient = client
+      return next(context)
+    }
+  }
+}
