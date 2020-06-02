3c3299523ad8174e60a9435924b7afe69c5b1168
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-06-01 21:14:35 -0700

    Install deps on run. Add bin/test.

--- examples/custom-middleware/boltzmann.js
+++ examples/custom-middleware/boltzmann.js
@@ -1,24 +1,27 @@
+#!/usr/bin/env node
 /* eslint-disable */
 /* istanbul ignore file */
+
 'use strict'
+
+const serviceName = (
+  process.env.SERVICE_NAME ||
+  require('./package.json').name.split('/').pop()
+)
 
 // 
 
-const ship = require('culture-ships').random()
+// 
 const querystring = require('querystring')
 const { promisify } = require('util')
 const isDev = require('are-we-dev')
 const fmw = require('find-my-way')
 const accepts = require('accepts')
 const http = require('http')
-const pino = require('pino')
+const bole = require('bole')
 const os = require('os')
 // 
 // 
-const pg = require('pg')
-// 
-// {% block requirements %}
-// {% endblock %}
 
 const THREW = Symbol.for('threw')
 const STATUS = Symbol.for('status')
@@ -85,7 +88,6 @@ class Context {
   constructor(request, response) {
     this.request = request
     this.start = Date.now()
-    this.params = null
 
     this.remote = request.socket
       ? request.socket.remoteAddress.replace('::ffff:', '')
@@ -102,22 +104,8 @@ class Context {
 
     // 
     // 
-    this._postgresPool = null
-    this._postgresConnection = null
-    // 
-    // {% block context_constructor %}
-    // {% endblock %}
   }
 
-  // {% block context_body %}
-  // {% endblock %}
-
-  // 
-  /** @type {Promise<pg.Client>} */
-  get postgresClient () {
-    this._postgresConnection = this._postgresConnection || this._postgresPool.connect()
-    return this._postgresConnection
-  }
   // 
 
   // 
@@ -189,11 +177,11 @@ function router (handlers) {
       }
 
       Object.assign(handler, {
+        ...rest,
         method,
         version,
         route,
         decorators: (decorators || []).map(xs => xs.name),
-        ...rest
       })
 
       wayfinder.on(method, route, opts, handler)
@@ -221,7 +209,6 @@ function router (handlers) {
       version
     } = match.handler
 
-    context.params = match.params
     context._routed = {
       method,
       route,
@@ -229,6 +216,7 @@ function router (handlers) {
       version,
       params: match.params
     }
+    context.params = match.params
 
     // 
       return match.handler(context, match.params, match.store, null)
@@ -256,7 +244,7 @@ async function buildMiddleware (middleware, router) {
   const result = middleware.reduce((lhs, rhs) => {
     const [mw, ...args] = Array.isArray(rhs) ? rhs : [rhs]
     return [...lhs, ...middlewareToSplice(mw), mw(...args)]
-  }, []).concat(middlewareToSplice())
+  }, []).concat(middlewareToSplice({ name: 'router' }))
 
   // 
   return result.reduceRight(async (lhs, rhs) => {
@@ -321,7 +309,7 @@ if (!isDev()) {
     if (isClosing) {
       process.exit(1)
     }
-    const logger = pino()
+    const logger = bole()
     logger.info('Caught SIGINT, preparing to shutdown. If running on the command line another ^C will close the app immediately.')
     isClosing = true
   })
@@ -385,8 +373,19 @@ function enforceInvariants () {
   }
 }
 
-function logging ({ logger = pino({ prettyPrint: isDev() }) } = {}) {
+function log ({
+  logger = bole(process.env.SERVICE_NAME || 'boltzmann'),
+  level = process.env.LOG_LEVEL || 'debug',
+  stream = process.stdout
+} = {}) {
   return function logMiddleware (next) {
+    if (isDev()) {
+      const pretty = require('bistre')({ time: true })
+      pretty.pipe(stream)
+      stream = pretty
+    }
+    bole.output({ level, stream })
+
     return async function inner (context) {
       const result = await next(context)
 
@@ -448,125 +447,14 @@ function urlEncoded (next) {
 // 
 // 
 // 
-function postgresMiddleware ({
-  url = process.env.PGURL || 'postgres://postgres@localhost:5432/todo-api',
-  max = Number(process.env.PGPOOLSIZE) || 20
-} = {}) {
-  return async next => {
-    const pool = new pg.Pool({
-      connectionString: url,
-      max
-    })
-
-    // make sure we can connect.
-    const testConn = await pool.connect()
-    testConn.release()
-
-    return async function postgres (context) {
-      context._postgresPool = pool
-      const isTransaction = context.method !== 'GET' && context.method !== 'HEAD'
-      if (isTransaction) {
-        const client = await context.postgresClient
-        await client.query('BEGIN;')
-      }
-
-      const result = await next(context)
-      if (context._postgresConnection) {
-        const client = await context._postgresConnection
-        if (isTransaction) {
-          await client.query(result[THREW] ? 'ROLLBACK' : 'COMMIT')
-        }
-        await client.release()
-      }
-      return result
-    }
-  }
-}
-// 
-
-function monitoringMiddleware ({
-  git = process.env.GIT_COMMIT,
-  reachability = {
-    // 
-    postgresReachability,
-    // 
-    // 
-    // 
-    // 
-    ..._requireOr('./reachability', {})
-  }
-} = {}) {
-  return next => {
-    const hostname = os.hostname()
-    let requestCount = 0
-    const statuses = {}
-    reachability = Object.entries(reachability)
-    return async function monitor (context) {
-      switch (context.url.pathname) {
-        case '/monitor/status':
-          const downstream = {}
-          for (const [name, test] of reachability) {
-            const meta = {status: 'failed', latency: 0, error: null}
-            const start = Date.now()
-            try {
-              await test(context, meta)
-              meta.status = 'healthy'
-            } catch (err) {
-              meta.error = err
-            } finally {
-              meta.latency = Date.now() - start
-            }
-            downstream[name] = meta
-          }
-
-          return {
-            git,
-            uptime: process.uptime(),
-            service: 'todo-api',
-            hostname,
-            memory: process.memoryUsage(),
-            downstream,
-            stats: {
-              requestCount,
-              statuses
-            }
-          }
-
-        default:
-          ++requestCount
-          const result = await next(context)
-          const body = result || {}
-          statuses[body[STATUS]] = statuses[body[STATUS]] || 0
-          ++statuses[body[STATUS]]
-          return result
-      }
-    }
-  }
-}
-
-function ping () {
-  return next => context => {
-    if (context.url.pathname === '/monitor/ping') {
-      return ship
-    }
-    return next(context)
-  }
-}
-
-// - - - - - - - - - - - - - - - -
-// Reachability Checks
-// - - - - - - - - - - - - - - - -
 
 // 
-async function postgresReachability (context, meta) {
-  const client = await context.postgresClient
-  meta.status = 'got-client'
-  await client.query('select 1;')
-}
+
 // 
 
 // 
 // 
+
 // 
 
 // - - - - - - - - - - - - - - - -
@@ -622,64 +510,21 @@ let savepointId = 0
 
 function test ({
   middleware = [],
-  handlers = _requireOr('./handlers', {}),
+  handlers = _requireOr('./handlers'),
   bodyParsers = _requireOr('./body', [urlEncoded, json]),
   after = require('tap').teardown
 }) {
   const shot = require('@hapi/shot')
   // 
-  const database = process.env.TEST_DB_NAME || 'todo-api_test'
-  const postgresClient = new pg.Client({
-    connectionString: `postgres://localhost:5432/${database}`
-  })
-  postgresClient.connect()
-  // 
 
   // 
 
   // 
-  // 
-
-  after(() => {
-    // 
-    postgresClient.end()
-    // 
-    // 
-    // 
-    // 
-  })
 
   return inner => async assert => {
     // 
-    // if we're in postgres, run the test in a transaction, run
-    // routes in checkpoints.
-    await postgresClient.query(`begin`)
-
-    middleware.push(() => next => async context => {
-      context._postgresConnection = postgresClient
-      const savepointname = `test_${process.pid}_${Date.now()}_${savepointId++}`
-      const isTransactional = context.method !== 'GET' && context.method !== 'HEAD'
-      if (isTransactional) {
-        await postgresClient.query(`savepoint ${savepointname}`)
-      }
-
-      const result = await next(context)
-      if (isTransactional) {
-        if ((result || {})[THREW]) {
-          await postgresClient.query(`rollback to savepoint ${savepointname}`)
-        } else {
-          await postgresClient.query(`release savepoint  ${savepointname}`)
-        }
-      }
-
-      return result
-    })
-    assert.postgresClient = postgresClient
-    // 
     // 
 
-    // 
-    // 
     const server = await main({ middleware, bodyParsers, handlers })
     const [onrequest] = server.listeners('request')
     const request = async ({
@@ -719,10 +564,6 @@ function test ({
       await inner(assert, request)
     } finally {
       // 
-      await postgresClient.query('rollback')
-      // 
-      // 
-      // 
     }
   }
 }
@@ -753,31 +594,6 @@ function _requireOr (target, value) {
   }
 }
 
-async function go(promiselike) {
-  try {
-    return [null, await promiselike]
-  } catch (err) {
-    return [err, null]
-  }
-}
-
-function err2http (type, status, headers = null) {
-  return (err) => {
-    if (err instanceof type) {
-      err[Symbol.for('status')] = status
-      if (headers) {
-        err[Symbol.for('headers')] = {
-          ...(err[Symbol.for('headers')] || {}),
-          headers
-        }
-      }
-    }
-    throw err
-  }
-}
-
-exports.go = go
-exports.err2http = err2http
 exports.Context = Context
 exports.main = main
 exports.body = {
@@ -785,16 +601,6 @@ exports.body = {
   urlEncoded
 }
 exports.decorators = {
-  err2http: (...config) => {
-    const cast = err2http(...config)
-    return next => async (...args) => {
-      try {
-        return next(...args)
-      } catch (err) {
-        cast(err)
-      }
-    }
-  },
   validate: {
     body: validateBody,
     query: validateBlock(ctx => ctx.query),
@@ -805,22 +611,15 @@ exports.decorators = {
 
 // 
 if (require.main === module) {
-  const runtimeMiddleware = [
-    // {% block runtime_middleware %}
-    // 
-    ping,
-    logging,
-    // 
-    // 
-    postgresMiddleware,
-    // 
-    // {% endblock %}
-  ]
   main({
     middleware: [
-      ...runtimeMiddleware,
+      // 
+      // 
+      log,
+      // 
+      // 
       ..._requireOr('./middleware', []),
-      ...[monitoringMiddleware]
+      // 
     ]
   }).then(server => {
     server.listen(Number(process.env.PORT) || 5000)
---
+++ examples/custom-middleware/handlers.js
@@ -0,0 +1,10 @@
+const { Context } = require('./boltzmann.js'); // optionally pull in typescript definition
+
+greeting.route = 'GET /hello/:name'
+async function greeting(/** @type {Context} */ context) {
+    return `hello ${context.params.name}`
+}
+
+module.exports = {
+    greeting
+}
