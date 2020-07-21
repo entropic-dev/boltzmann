a5449b1c42d83a4d7dac5913c104e44f5e92320d
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-07-21 11:56:57 -0700

    fix: CORS middleware handles "simple" requests now
    Roz and Kyle discovered a failing case in the CORS middleware, where
    fetch() was making "simple" [1] style requests which require the
    CORS headers to be present on the resource response. We now include
    those headers and also pick the appropriate origin from the list.
    
    [1]: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#Simple_requests

---
+++ examples/attaching-middleware/handlers.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const { Context } = require('./boltzmann.js') // optionally pull in typescript definition
+const { log } = require('./middleware')
+
+base.route = 'GET /'
+async function base(/** @type {Context} */ context) {
+  return 'ok!'
+}
+
+greeting.route = 'GET /hello/:name'
+// This middleware will only fire when the application receives a request
+// for this route.
+greeting.middleware = [
+  log, // use the default params,
+  [log, { before: 'hello', after: 'goodbye' }] // or customize!
+]
+async function greeting(/** @type {Context} */ context) {
+  return `hello ${context.params.name}`
+}
+
+module.exports = {
+  base,
+  greeting,
+}
