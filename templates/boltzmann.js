b186b2ebb2fbf164f0824d3f59f063d5db4d82c6
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-07-21 11:56:57 -0700

    fix: CORS middleware handles "simple" requests now
    Roz and Kyle discovered a failing case in the CORS middleware, where
    fetch() was making "simple" [1] style requests which require the
    CORS headers to be present on the resource response. We now include
    those headers and also pick the appropriate origin from the list.
    
    [1]: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#Simple_requests

--- templates/boltzmann.js
+++ templates/boltzmann.js
@@ -548,23 +548,36 @@ function template ({
 // {% endif %}
 
 function handleCORS ({
-  origins = [],
-  methods = [],
-  headers = []
+  origins = isDev() ? '*' : String(process.env.CORS_ALLOW_ORIGINS).split(','),
+  methods = String(process.env.CORS_ALLOW_METHODS).split(','),
+  headers = String(process.env.CORS_ALLOW_HEADERS).split(',')
 }) {
+  origins = [].concat(origins)
+  const includesStar = origins.includes('*')
+
   return next => {
-    return function cors (context) {
-      if (context.request.method === 'OPTIONS') {
-        return {
-          [Symbol.for('headers')]: {
-            'Access-Control-Allow-Origin': origins,
-            'Access-Control-Allow-Methods': methods,
-            'Access-Control-Allow-Headers': headers
-          }
-        }
+    return async function cors (context) {
+      if (!includesStar && !origins.includes(context.headers.origin)) {
+        throw Object.assign(new Error('Origin not allowed'), {
+          [Symbol.for('status')]: 400
+        })
       }
 
-      return next(context)
+      const response = (
+        context.method === 'OPTIONS'
+        ? Object.assign(Buffer.from(''), {
+            [Symbol.for('status')]: 204,
+          })
+        : await next(context)
+      )
+
+      response[Symbol.for('headers')] = {
+        'Access-Control-Allow-Origin': includesStar ? '*' : context.headers.origin,
+        'Access-Control-Allow-Methods': [].concat(methods).join(','),
+        'Access-Control-Allow-Headers': [].concat(headers).join(',')
+      }
+
+      return response
     }
   }
 }
