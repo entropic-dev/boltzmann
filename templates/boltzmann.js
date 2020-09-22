45749a77bdaf1dd38b883e027fb37b8d85bd1452
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-09-22 16:50:30 -0700

    reintroduce mysteriously-missing Session class

--- templates/boltzmann.js
+++ templates/boltzmann.js
@@ -2172,6 +2172,39 @@ class Cookie extends Map {
   }
 }
 
+class BadSessionErrror extends Error {
+  [STATUS] = 400
+}
+
+class Session extends Map {
+  constructor(id, ...args) {
+    super(...args)
+    this.dirty = false
+    this.id = id
+  }
+
+  reissue() {
+    this[REISSUE] = true
+  }
+
+  set(key, value) {
+    const old = this.get(key)
+    if (value === old) {
+      return super.set(key, value)
+    }
+    this.dirty = true
+    return super.set(key, value)
+  }
+
+  delete(key) {
+    if (!this.has(key)) {
+      return super.delete(key)
+    }
+    this.dirty = true
+    return super.delete(key)
+  }
+}
+
 {{ EXPORTS }} const body = {
   json,
   urlEncoded
