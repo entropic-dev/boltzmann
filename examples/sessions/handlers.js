ac840f319674a36d891f49200c886d51ca55286f
C J Silverio <ceejceej@gmail.com>
2020-09-23 11:46:31 -0700

    Add more comments to the example code.
    Also, auto-format the html template.

--- examples/sessions/handlers.js
+++ examples/sessions/handlers.js
@@ -11,11 +11,17 @@ async function index(/** @type {Context} */ context, { errors } = {}) {
   return {
     [Symbol.for('template')]: 'index.html',
     errors,
+    // Here we generate and use a random token to protect against
+    // cross-site request forgery. The middleware will set a cookie for
+    // us and we'll compare to the value that gets submitted with the form.
     token: context.csrfToken(),
   }
 }
 
 login.route = 'POST /login'
+// Here's an example of validating user input in a body.
+// The csrf protection middleware will take care of looking
+// for a _csrf parameter for us.
 login.decorators = [
   validate.body({
     type: 'object',
@@ -27,19 +33,23 @@ login.decorators = [
   }),
 ]
 async function login(/** @type {Context} */ context) {
+  // We await the body because we want to make sure we've read all of it.
   const { form_username: username, form_password: password } = await context.body
 
-  // there is only one password. this is very secure!
+  // There is only one password. This is very secure!
   if (password !== 'CATSROOL') {
     return index(context, { errors: ['Invalid password'] })
   }
 
+  // Loading the session requires I/O to redis, so it's async.
   const session = await context.session
   logger.info(`storing session for ${username}!`)
+  // Using the session object once we have it is synchronous.
   session.set('user', {
     username,
   })
 
+  // Now we respond to the browser with a redirect.
   return {
     [Symbol.for('status')]: 301,
     [Symbol.for('headers')]: {
