4c9f9da5df1f32f959fc21fccdab43a2a7ccb4e9
Chris Dickinson <chris@neversaw.us>
2021-04-13 16:36:37 -0700

    tsify main

---
+++ templates/boltzmann/main.ts
@@ -0,0 +1,66 @@
+// {% if selftest %}
+import bole from '@entropic-dev/bole'
+import isDev from 'are-we-dev'
+
+import { main } from './bin/main'
+import { Handler, MiddlewareConfig } from './core/middleware'
+import { Context } from './data/context'
+import { trace } from './middleware/honeycomb'
+import { livereload } from './middleware/livereload'
+import { log } from './middleware/log'
+import { handlePing } from './middleware/ping'
+import { attachPostgres } from './middleware/postgres'
+import { attachRedis } from './middleware/redis'
+import { handleStatus } from './middleware/status'
+import { _processMiddleware, _requireOr } from './utils'
+// {% endif %}
+
+/* istanbul ignore next */
+if (require.main === module) {
+  function passthrough() {
+    return (next: Handler) => (context: Context) => next(context)
+  }
+
+  main({
+    middleware: (_requireOr('./middleware', [] as MiddlewareConfig[]) as Promise<MiddlewareConfig[]>)
+      .then(_processMiddleware)
+      .then((mw) =>
+        [
+          // {% if honeycomb %}
+          trace ,
+          // {% endif %}
+          // {% if ping %}
+          handlePing ,
+          // {% endif %}
+          // {% if livereload %}
+          (isDev() ? livereload : passthrough) ,
+          // {% endif %}
+          log ,
+
+          // {% if redis %}
+          attachRedis ,
+          // {% endif %}
+          // {% if postgres %}
+          attachPostgres ,
+          // {% endif %}
+          ...mw,
+          // {% if status %}
+          ...[handleStatus ],
+          // {% endif %}
+        ].filter(Boolean)
+      ),
+  })
+    .then((server) => {
+      server.listen(Number(process.env.PORT) || 5000, () => {
+        const addrinfo = server.address()
+        if (!addrinfo) {
+          return
+        }
+        bole('boltzmann:server').info(`now listening on port ${typeof addrinfo == 'string' ? addrinfo : addrinfo.port}`)
+      })
+    })
+    .catch((err) => {
+      console.error(err.stack)
+      process.exit(1)
+    })
+}
