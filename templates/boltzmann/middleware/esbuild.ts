9253fca6cde01ee86cbe9fccc9365651e514cc24
Chris Dickinson <chris@neversaw.us>
2021-04-12 21:34:50 -0700

    wip: esbuild middleware

--- templates/boltzmann/middleware/esbuild.ts
+++ templates/boltzmann/middleware/esbuild.ts
@@ -1,5 +1,21 @@
-let _build = null
-function esbuild({
+// {% if selftest %}
+import bole from '@entropic-dev/bole'
+import { build } from 'esbuild'
+import isDev from 'are-we-dev'
+import crypto from 'crypto'
+import path from 'path'
+import os from 'os'
+
+import { promises as fs } from 'fs'
+import { staticfiles } from '../middleware/staticfiles'
+import { _findESBuildEntries } from '../bin/esbuild'
+import { Handler } from '../core/middleware'
+import { Context } from '../data/context'
+import { routes } from '../core/routes'
+import { _requireOr } from '../utils'
+// {% endif %}
+
+/* {% if selftest %} */export /* {% endif %} */function esbuild({
   source = 'client',
   prefix = '_assets',
   staticUrl = process.env.STATIC_URL,
@@ -8,20 +24,23 @@ function esbuild({
 } = {}) {
   const logger = bole('boltzmann:esbuild')
   if (!isDev()) {
-    return next => staticAssets(async function inner(context) {
-      const response = await next(context)
-      if (!response[Symbol.for('template')]) {
-        return response
-      }
+    return async (next: Handler) => {
+      const entries = await _findESBuildEntries(source)
+      return staticAssets(async function inner(context: Context) {
+        const response = await next(context)
+        if (!response[Symbol.for('template')]) {
+          return response
+        }
 
-      const entry = entries.get(context.handler)
-      if (!entry) {
-        return response
-      }
+        const entry = entries.get(context.handler)
+        if (!entry) {
+          return response
+        }
 
-      response.ESBUILD_ENTRY_URL = `/${prefix}/${entry.replace(/\\/g, '/')}`
-      return response
-    })
+        response.ESBUILD_ENTRY_URL = `/${prefix}/${entry.replace(/\\/g, '/')}`
+        return response
+      })
+    }
   }
 
   const staticAssets = staticfiles({
@@ -31,17 +50,16 @@ function esbuild({
     quiet: true
   })
 
-  return async (next) => {
+  return async (next: Handler) => {
     await fs.mkdir(destination, { recursive: true })
 
-    const routeMetadata = [...routes(await _requireOr('./handlers'))]
+    const _routeMetadata = [...routes(await _requireOr('./handlers', {}))]
 
     const entries = await _findESBuildEntries(source)
 
     const start = Date.now()
     if (entries.size > 0) {
-      _build = _build || require('esbuild').build
-      await _build({
+      await build({
         sourcemap: 'inline',
         define: {
           'process.env': 'false',
@@ -57,7 +75,7 @@ function esbuild({
     }
     logger.info(`esbuild development middleware active; built in ${Date.now() - start}ms`)
 
-    return staticAssets(async function inner(context) {
+    return staticAssets(async function inner(context: Context) {
       const response = await next(context)
       if (!response[Symbol.for('template')]) {
         return response
