ec6a8f7e4e623cae786d5e363942b90cb00faff1
Chris Dickinson <christopher.s.dickinson@gmail.com>
2021-04-29 21:51:04 -0700

    add redis mw test

--- templates/boltzmann/middleware/redis.ts
+++ templates/boltzmann/middleware/redis.ts
@@ -3,7 +3,7 @@ export { attachRedis }
 
 import { Handler } from '../core/middleware'
 import { Context } from '../data/context'
-import redis from 'handy-redis'
+import * as redis from 'handy-redis'
 void `{% endif %}`;
 
 /**{{- tsdoc(page="03-middleware.md", section="attachredis") -}}*/
@@ -17,3 +17,31 @@ function attachRedis ({ url = process.env.REDIS_URL } = {}) {
   }
 }
 
+void `{% if selftest %}`;
+import tap from 'tap'
+import { createServer } from 'net'
+/* c8 ignore next */
+if (require.main === module) {
+  const { test } = tap
+
+  test('attachRedis', async (assert) => {
+    const server = createServer().listen(6379)
+    assert.teardown(async () => {
+      server.close()
+    })
+
+    test('attaches client', async (assert) => {
+      const adaptor = attachRedis()
+      const handler = adaptor(c => c)
+
+      const context: Record<string, any> = {}
+      const client = redis.createHandyClient({})
+      client.end(false)
+      const response = await handler(<any>context)
+      assert.equal(response, context)
+      assert.type(context._redisClient, <any>client.constructor)
+      context._redisClient.end(false)
+    })
+  })
+}
+void `{% endif %}`;
