5f87c02d682712032cf2460134b12cdee8083f07
Chris Dickinson <chris@neversaw.us>
2021-05-04 16:58:19 -0700

    attempt to crossplat

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 use std::process::Command;
 
 fn main() {
-    #[cfg(not(target_os = "windows"))]
-    Command::new("./bin/buildjs.sh").output().expect("failed to build javascript");
+    Command::new("bash")
+        .args(&["./bin/buildjs.sh"])
+        .output()
+        .expect("failed to build javascript");
 }
