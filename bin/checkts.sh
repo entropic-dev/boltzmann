80345df7cc17edebc8283354e4561207590fd32c
Josh Holbrook <jholbrook@eaze.com>
2022-01-19 17:11:31 -0500

    Simplify ts check script

--- bin/checkts.sh
+++ bin/checkts.sh
@@ -2,24 +2,6 @@
 
 set -e
 
-function get_platform() {
-  if [ "$OSTYPE" == "linux-gnu" ]; then
-    echo linux
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo darwin
-  elif [ "$OSTYPE" == "cygwin" ]; then
-    echo windows
-  elif [ "$OSTYPE" == "msys" ]; then
-    echo windows
-  elif [ "$OSTYPE" == "win32" ]; then
-    echo windows
-  else
-    echo "Cannot detect OS" >&2
-    exit 1
-  fi
-}
-plat=$(get_platform)
-
 if [ ! -e node_modules ]; then
   npm ci
 else
